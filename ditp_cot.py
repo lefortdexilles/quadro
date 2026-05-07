0ad9eeb202fdcf86186a61a71ce137265e90bbdc
renelefortdexille <123302615+lefortdexilles@users.noreply.github.com>
2026-05-07 18:12:58 +0200

    Refactor ditp_cot.py to include openpyxl

--- ditp_cot.py
+++ ditp_cot.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import numpy as np
-
+import openpyxl
 
 st.set_page_config(page_title="Priorisation bilatérale", layout="wide")
 
